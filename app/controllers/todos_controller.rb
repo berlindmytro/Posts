845525f6ead2693480abdc5496058ba63ada70f7
Dmitry <berlindmytro@gmail.com>
2022-04-21 12:45:35 +0300

    created TodosController & /todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@
 
 class TodosController < ApplicationController
   def index
-    @todo = Todo.all
+    @todos = Todo.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "/articles", to: "articles#index"
+
   root 'todos#index', as: 'home'
   get 'todos' => 'pages#todopage', as: 'todos'
   resources :todos
   resources :pages
+end
+
+Rails.application.routes.draw do
+  get "/posts", to: "posts#index"
 end
